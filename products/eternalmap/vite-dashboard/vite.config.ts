a3babeae012bc94b2bb650be06a3f11e95cda265
Summerlyn Turner <summerlyntds@gmail.com>
2026-06-16 11:51:23 -0700

    Rebuild dashboard base paths from /tools/{product}/ to /{product}/ for app.basemapped.com deployment

--- products/eternalmap/vite-dashboard/vite.config.ts
+++ products/eternalmap/vite-dashboard/vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 
 export default defineConfig({
-  base: '/tools/eternalmap/',
+  base: '/eternalmap/',
   plugins: [react()],
   resolve: {
     alias: {
